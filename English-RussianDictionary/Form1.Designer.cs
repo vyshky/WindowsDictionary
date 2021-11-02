4eabaeb87017efdf1d93231a5f241a648b74ddda
Alexander <vyshky2@gmail.com>
2021-11-02 13:56:40 +0400

    add debug buld

--- English-RussianDictionary/Form1.Designer.cs
+++ English-RussianDictionary/Form1.Designer.cs
@@ -154,7 +154,6 @@ namespace English_RussianDictionary
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(186, 112);
             this.panel2.TabIndex = 0;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // label2
             // 
---
+++ English-RussianDictionary/buldVersion1/dictionary/Russian.dsl
@@ -0,0 +1,4 @@
+Дом	Home;
+Земля	Earth;
+Перевод	Translation;
+Привет мир	Hello World;Mir privet;Privet mir;
